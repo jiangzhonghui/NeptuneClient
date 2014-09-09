4e9e5f9268d68228df78bf232c9a5e8300c24b43
jython234 <jython234@hotmail.com>
2014-09-08 19:15:32 -0500

    Uploaded source for the project.

---
+++ src/neptune/client/NeptuneClient.java
@@ -0,0 +1,11 @@
+package neptune.client;
+
+public class NeptuneClient {
+
+	public static void main(String[] args) {
+		// Main Startup file for the Client.
+		System.out.println("Harro!"); //For now...
+
+	}
+
+}
